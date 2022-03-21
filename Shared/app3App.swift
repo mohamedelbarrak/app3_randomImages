ce1078536611f10629a6a34abe47878c3bc153c8
mohamedelbarrak <mohamedelbarrak@outlook.fr>
2022-03-21 10:18:36 +0100

    Initial Commit

---
+++ Shared/app3App.swift
@@ -0,0 +1,17 @@
+//
+//  app3App.swift
+//  Shared
+//
+//  Created by E-challenge 2022 on 21/3/2022.
+//
+
+import SwiftUI
+
+@main
+struct app3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
